7b9580d6dda573b3a2ae6518768d0d6d144917ff
Stefan Zimmermann <szimmermann@advaoptical.com>
2017-07-17 16:39:41 +0200

    setup: Update requires and packages

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ setup(
     version='0.1.dev',
     description="Akka code stub generator from YANG models via pyang.",
 
-    install_requires=['pyang', 'jinja2', 'path.py'],
+    install_requires=['pyang', 'jinja2', 'path.py', 'pluggy>=0.4'],
 
     packages=[
         'alpakka',
-        'alpakka.wrapper',
         'alpakka.pyang_plugins',
+        'alpakka.wrapper',
+        'alpakka.wools',
+        'alpakka.templates',
     ],
     entry_points={
         'console_scripts': ['alpakka=alpakka:run'],
