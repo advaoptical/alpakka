082aa7c4c5ab4a31d975cbcced089a396093f3d0
Stefan Zimmermann <szimmermann@advaoptical.com>
2018-08-01 16:16:06 +0200

    Improve setup

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
+
 setup(
     name='alpakka',
-    version='0.1.dev',
     description="Cutest YANG Eater alive",
 
+    author="ADVA Optical Networking",
+    maintainer="Thomas Szyrkowiec",
+    maintainer_email="tszyrkowiec@advaoptical.com",
+
+    license="Apache License 2.0",
+
+    setup_requires=open('requirements.setup.txt'),
     install_requires=['pyang', 'path.py', 'ipython'],
+
+    use_scm_version={'local_scheme': lambda _: ''},
 
     packages=[
         'alpakka',
@@ -16,4 +25,24 @@ setup(
     entry_points={
         'console_scripts': ['alpakka=alpakka:run'],
     },
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Utilities',
+    ],
+    keywords=[
+        'alpakka', 'yang', 'pyang', 'wool', 'wools',
+    ],
 )
